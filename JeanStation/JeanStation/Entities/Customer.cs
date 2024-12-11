fe9c5af139315625a61e9562ddbe8d8e39009b1c
SubharajGhosh <ghoshsubharajani34@gmail.com>
2024-12-11 16:04:16 +0530

    commit2

---
+++ JeanStation/JeanStation/Entities/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace JeanStation.Entities
+{
+    public class Customer
+    {
+        public string CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string Email {  get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        [ForeignKey("UserNavigation")]
+        public string UserId { get; set; }
+        public User UserNavigation { get; set; }
+
+    }
+}
---
+++ JeanStation/JeanStation/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JeanStation.Entities
+{
+    public class User
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Role {  get; set; }
+    }
+}
