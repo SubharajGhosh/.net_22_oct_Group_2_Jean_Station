3cf1d0a696f9acc774d162f7c31567b2717ea93b
Jaybhai007 <007jaybhaipatel@gmail.com>
2024-12-11 16:33:07 +0530

    commitjay

---
+++ JeanStation/JeanStation/Entities/OrderItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JeanStation.Entities
+{
+    public class OrderItem
+    {
+        [Required]
+        public string OrderItemId {  get; set; }
+        public int Quantity { get; set; }
+
+        [Foreignkey("OrderNavigation")]
+
+        public string OrderId {  get; set; }
+
+        public int UnitPrice { get; set; }  
+
+        public int TotalPrice { get; set; }
+
+        [Foreignkey("JeanNavigation")]
+
+        public string JeansId { get; set; }
+
+        public Order OrderNavigation { get; set; }
+        public Jeans JeanNavigation { get; set; }
+
+
+
+
+
+
+
+    }
+}
