4da5db23240d6b705df68d041a4d2081fbfc92ff
Deepakdeepu1919 <deepakdeepu1919@gmail.com>
2024-12-11 17:20:38 +0530

    commitDeepak1

---
+++ JeanStation/JeanStation/Entities/PaymentDetails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JeanStation.Entities
+{
+    public class PaymentDetails
+    {
+        [Key]
+        public string PaymentId {  get; set; }
+        public string PaymentMode {  get; set; }
+        public string PaymentStatus { get; set; }
+        [ForeignKey("OrderNavigation")]
+        public string OrderId {  get; set; }
+        public Order OrderNavigation { get; set; }
+        
+    }
+}
